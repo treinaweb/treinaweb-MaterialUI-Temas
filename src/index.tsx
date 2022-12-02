b571230a6f44d0323ca90c0a40f41a4ddc8f60ea
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-12-02 15:53:07 -0300

    05:21 - Definindo tema claro e escuro

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/App";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import theme from "./theme/default";
+import themeDark from "./theme/theme-dark";
+import themeLight from "./theme/theme-light";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={themeDark}>
       <CssBaseline />
       <App />
     </ThemeProvider>
